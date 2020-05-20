877c39c0c499431e561f1be6b0a461e804fd6929
matricce <ruviansanches20@gmail.com>
2020-05-20 17:13:50 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 var music = document.getElementById("myMusic");
 var music_state = 1;
-var scaleFirst = "scale(1.07)"
-var scaleSecond = "scale(1.035)";
-var scaleThird = "scale(1.012)";
-var scaleDefault = "scale(1)";
+var scaleFirst = "translate(-1.2rem)"
+var scaleSecond = "translate(-.6rem)";
+var scaleThird = "translate(-.3rem)";
+var scaleDefault = "translate(0)";
+// var scaleFirst = "scale(1.07)";
+// var scaleSecond = "scale(1.04)";
+// var scaleThird = "scale(1.02)";
+// var scaleDefault = "scale(1)";
 music.volume = 0.3;
 music.loop = true;
 function musicControl() {
@@ -18,28 +22,43 @@ function musicControl() {
   }
 }
 function scaleThis(element) {
-  if(element.previousElementSibling) {
-    element.previousElementSibling.style.transform = scaleSecond;
-    if(element.previousElementSibling.previousElementSibling)
-    element.previousElementSibling.previousElementSibling.style.transform = scaleThird;
+  var secondLayerP = element.previousElementSibling ? element.previousElementSibling : false;
+  var secondLayerN = element.nextElementSibling ? element.nextElementSibling : false;
+  var thirdLayerP = secondLayerP.previousElementSibling ? secondLayerP.previousElementSibling : false;
+  var thirdLayerN = secondLayerN.nextElementSibling ? secondLayerN.nextElementSibling : false;
+  if(secondLayerP) {
+    secondLayerP.style.transition = ".6s";
+    secondLayerP.style.transform = scaleSecond;
+    if(thirdLayerP) {
+      thirdLayerP.style.transition = ".9s";
+      thirdLayerP.style.transform = scaleThird;
+    }
   }
-  if(element.nextElementSibling) {
-    element.nextElementSibling.style.transform = scaleSecond;
-    if(element.nextElementSibling.nextElementSibling)
-    element.nextElementSibling.nextElementSibling.style.transform = scaleThird;
+  if(secondLayerN) {
+    secondLayerN.style.transition = ".6s";
+    secondLayerN.style.transform = scaleSecond;
+    if(thirdLayerN) {
+      thirdLayerN.style.transition = ".9s";
+      thirdLayerN.style.transform = scaleThird;
+    }
   }
+  element.style.transition = ".3s";
   element.style.transform = scaleFirst;
 }
 function noscaleThis(element) {
-  if(element.previousElementSibling) {
-    element.previousElementSibling.style.transform = scaleDefault;
-    if(element.previousElementSibling.previousElementSibling)
-    element.previousElementSibling.previousElementSibling.style.transform = scaleDefault;
+  var secondLayerP = element.previousElementSibling ? element.previousElementSibling : false;
+  var secondLayerN = element.nextElementSibling ? element.nextElementSibling : false;
+  var thirdLayerP = secondLayerP.previousElementSibling ? secondLayerP.previousElementSibling : false;
+  var thirdLayerN = secondLayerN.nextElementSibling ? secondLayerN.nextElementSibling : false;
+  if(secondLayerP) {
+    secondLayerP.style.transform = scaleDefault;
+    if(thirdLayerP)
+    thirdLayerP.style.transform = scaleDefault;
   }
-  if(element.nextElementSibling) {
-    element.nextElementSibling.style.transform = scaleDefault;
-    if(element.nextElementSibling.nextElementSibling)
-    element.nextElementSibling.nextElementSibling.style.transform = scaleDefault;
+  if(secondLayerN) {
+    secondLayerN.style.transform = scaleDefault;
+    if(thirdLayerN)
+    thirdLayerN.style.transform = scaleDefault;
   }
   element.style.transform = scaleDefault;
 }
