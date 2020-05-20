e046c5072d5c9de77ae08ce135b4a459d3592ffc
matricce <ruviansanches20@gmail.com>
2020-05-20 16:27:16 +0000

    ..

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 var music = document.getElementById("myMusic");
 var music_state = 1;
+var scaleFirst = "scale(1.07)"
+var scaleSecond = "scale(1.035)";
+var scaleThird = "scale(1.012)";
+var scaleDefault = "scale(1)";
 music.volume = 0.3;
 music.loop = true;
 function musicControl() {
@@ -12,4 +16,30 @@ function musicControl() {
     document.getElementById("btn-music").innerHTML = "volume_off";
     music.pause();
   }
+}
+function scaleThis(element) {
+  if(element.previousElementSibling) {
+    element.previousElementSibling.style.transform = scaleSecond;
+    if(element.previousElementSibling.previousElementSibling)
+    element.previousElementSibling.previousElementSibling.style.transform = scaleThird;
+  }
+  if(element.nextElementSibling) {
+    element.nextElementSibling.style.transform = scaleSecond;
+    if(element.nextElementSibling.nextElementSibling)
+    element.nextElementSibling.nextElementSibling.style.transform = scaleThird;
+  }
+  element.style.transform = scaleFirst;
+}
+function noscaleThis(element) {
+  if(element.previousElementSibling) {
+    element.previousElementSibling.style.transform = scaleDefault;
+    if(element.previousElementSibling.previousElementSibling)
+    element.previousElementSibling.previousElementSibling.style.transform = scaleDefault;
+  }
+  if(element.nextElementSibling) {
+    element.nextElementSibling.style.transform = scaleDefault;
+    if(element.nextElementSibling.nextElementSibling)
+    element.nextElementSibling.nextElementSibling.style.transform = scaleDefault;
+  }
+  element.style.transform = scaleDefault;
 }
