c2304d4ed77d93acec6c5d2ac312640581c1e8a9
Your Name <you@example.com>
2023-05-21 13:37:00 -0300

    refactor: melhorias na apresentação

--- js/script.js
+++ js/script.js
@@ -1,64 +1,73 @@
-var music = document.getElementById("myMusic");
-var music_state = 1;
-var scaleFirst = "translate(-1.2rem)"
-var scaleSecond = "translate(-.6rem)";
-var scaleThird = "translate(-.3rem)";
-var scaleDefault = "translate(0)";
-// var scaleFirst = "scale(1.07)";
-// var scaleSecond = "scale(1.04)";
-// var scaleThird = "scale(1.02)";
-// var scaleDefault = "scale(1)";
-music.volume = 0.3;
-music.loop = true;
+const music = document.getElementById("myMusic")
+const music_state = 1
+const scaleFirst = "translate(-5rem)"
+const scaleSecond = "translate(-3rem)"
+const scaleThird = "translate(-1.8rem)"
+const scaleDefault = "translate(0)"
+music.volume = 0.3
+music.loop = true
 function musicControl() {
-  music_state = !music_state;
+  music_state = !music_state
   if (!music_state) {
-    document.getElementById("btn-music").innerHTML = "volume_up";
-    music.play();
-  }
-  else {
-    document.getElementById("btn-music").innerHTML = "volume_off";
-    music.pause();
+    document.getElementById("btn-music").innerHTML = "volume_up"
+    music.play()
+  } else {
+    document.getElementById("btn-music").innerHTML = "volume_off"
+    music.pause()
   }
 }
 function scaleThis(element) {
-  var secondLayerP = element.previousElementSibling ? element.previousElementSibling : false;
-  var secondLayerN = element.nextElementSibling ? element.nextElementSibling : false;
-  var thirdLayerP = secondLayerP.previousElementSibling ? secondLayerP.previousElementSibling : false;
-  var thirdLayerN = secondLayerN.nextElementSibling ? secondLayerN.nextElementSibling : false;
-  if(secondLayerP) {
-    secondLayerP.style.transition = ".6s";
-    secondLayerP.style.transform = scaleSecond;
-    if(thirdLayerP) {
-      thirdLayerP.style.transition = ".9s";
-      thirdLayerP.style.transform = scaleThird;
+  const secondLayerP = element.previousElementSibling
+    ? element.previousElementSibling
+    : false
+  const secondLayerN = element.nextElementSibling
+    ? element.nextElementSibling
+    : false
+  const thirdLayerP = secondLayerP.previousElementSibling
+    ? secondLayerP.previousElementSibling
+    : false
+  const thirdLayerN = secondLayerN.nextElementSibling
+    ? secondLayerN.nextElementSibling
+    : false
+  if (secondLayerP) {
+    secondLayerP.style.transition = ".6s"
+    secondLayerP.style.transform = scaleSecond
+    if (thirdLayerP) {
+      thirdLayerP.style.transition = ".9s"
+      thirdLayerP.style.transform = scaleThird
     }
   }
-  if(secondLayerN) {
-    secondLayerN.style.transition = ".6s";
-    secondLayerN.style.transform = scaleSecond;
-    if(thirdLayerN) {
-      thirdLayerN.style.transition = ".9s";
-      thirdLayerN.style.transform = scaleThird;
+  if (secondLayerN) {
+    secondLayerN.style.transition = ".6s"
+    secondLayerN.style.transform = scaleSecond
+    if (thirdLayerN) {
+      thirdLayerN.style.transition = ".9s"
+      thirdLayerN.style.transform = scaleThird
     }
   }
-  element.style.transition = ".3s";
-  element.style.transform = scaleFirst;
+  element.style.transition = ".3s"
+  element.style.transform = scaleFirst
 }
 function noscaleThis(element) {
-  var secondLayerP = element.previousElementSibling ? element.previousElementSibling : false;
-  var secondLayerN = element.nextElementSibling ? element.nextElementSibling : false;
-  var thirdLayerP = secondLayerP.previousElementSibling ? secondLayerP.previousElementSibling : false;
-  var thirdLayerN = secondLayerN.nextElementSibling ? secondLayerN.nextElementSibling : false;
-  if(secondLayerP) {
-    secondLayerP.style.transform = scaleDefault;
-    if(thirdLayerP)
-    thirdLayerP.style.transform = scaleDefault;
+  const secondLayerP = element.previousElementSibling
+    ? element.previousElementSibling
+    : false
+  const secondLayerN = element.nextElementSibling
+    ? element.nextElementSibling
+    : false
+  const thirdLayerP = secondLayerP.previousElementSibling
+    ? secondLayerP.previousElementSibling
+    : false
+  const thirdLayerN = secondLayerN.nextElementSibling
+    ? secondLayerN.nextElementSibling
+    : false
+  if (secondLayerP) {
+    secondLayerP.style.transform = scaleDefault
+    if (thirdLayerP) thirdLayerP.style.transform = scaleDefault
   }
-  if(secondLayerN) {
-    secondLayerN.style.transform = scaleDefault;
-    if(thirdLayerN)
-    thirdLayerN.style.transform = scaleDefault;
+  if (secondLayerN) {
+    secondLayerN.style.transform = scaleDefault
+    if (thirdLayerN) thirdLayerN.style.transform = scaleDefault
   }
-  element.style.transform = scaleDefault;
+  element.style.transform = scaleDefault
 }
